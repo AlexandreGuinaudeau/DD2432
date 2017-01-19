86cb8974ced201e2bdc34ff68564f19f6f63ab99
AlexandreGuinaudeau <alexandre.guinaudeau@gmail.com>
2017-01-19 16:17:49 +0100

    Corrected cast to int in nsepdata

--- Lab1/nsepdata.m
+++ Lab1/nsepdata.m
@@ -3,8 +3,8 @@ function [patterns, targets] = nsepdata(ndata)
 % Initialization of the data points 
 
 %two differents classes of 2d points
-classA(1,:) = [randn(1,ndata/2) .* 0.2 - 1.0, ...
-                   randn(1,(ndata+1)/2) .* 0.2 + 1.0];
+classA(1,:) = [randn(1,floor(ndata/2)) .* 0.2 - 1.0, ...
+                   randn(1,ceil(ndata/2)) .* 0.2 + 1.0];
 classA(2,:) = randn(1,ndata) .* 0.2 + 0.3;
 classB(1,:) = randn(1,ndata) .* 0.3 + 0.0;
 classB(2,:) = randn(1,ndata) .* 0.3 - 0.1;
